79f4c2df2339eb4e43006b43b373224366ff3c51
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-11-10 10:47:24 +0700

    helper latlong

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,3 +1,11 @@
 <?php
 
-// 
+function valid_latlong($lat, $long)
+{
+    if (!is_numeric($lat) || !is_numeric($long)) {
+        return false;
+    }
+    $lat = floatval($lat);
+    $long = floatval($long);
+    return ($lat >= -90 && $lat <= 90) && ($long >= -180 && $long <= 180);
+}
--- app/Models/Spaldt.php
+++ app/Models/Spaldt.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Spaldt extends Model
 {
     protected $guarded = [];
+    protected $appends = ['is_valid_map'];
+
+    public function getIsValidMapAttribute(): bool
+    {
+        return valid_latlong($this->lat, $this->long);
+    }
 }
