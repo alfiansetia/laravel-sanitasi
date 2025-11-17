b4742a468cae0aee024014fe13c518ac2078d11d
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-11-17 11:43:24 +0700

    update data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,87 +1,101 @@
 @extends('layouts.template')
 
+@push('css')
+    <style>
+        .apexcharts-menu {
+            z-index: 9999 !important;
+        }
+
+        .apexcharts-toolbar {
+            z-index: 50 !important;
+        }
+
+        .apexcharts-menu.open {
+            background: white;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+        }
+
+        .stats-icon {
+            width: 3rem;
+            height: 3rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 0.75rem;
+            color: white;
+        }
+
+        /* Warna custom */
+        .stats-icon.tpa {
+            background-color: #003A70;
+            /* Biru gelap */
+        }
+
+        .stats-icon.tpst {
+            background-color: #F7C904;
+            /* Kuning */
+            color: black !important;
+        }
+
+        .stats-icon.tps3r {
+            background-color: #C1272D;
+            /* Merah */
+        }
+
+        .stats-icon.iplt {
+            background-color: #4A90E2;
+            /* Biru */
+        }
+
+        .stats-icon.spald {
+            background-color: #F5A623;
+            /* Orange */
+            color: black !important;
+        }
+    </style>
+@endpush
+
 @section('content')
     <section class="row">
-        <div class="col-12 col-lg-9">
-            <div class="row">
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                    <div class="stats-icon purple mb-2">
-                                        <i class="iconly-boldShow"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold">Profile Views</h6>
-                                    <h6 class="font-extrabold mb-0">112.000</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                    <div class="stats-icon blue mb-2">
-                                        <i class="iconly-boldProfile"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold">Followers</h6>
-                                    <h6 class="font-extrabold mb-0">183.000</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                    <div class="stats-icon green mb-2">
-                                        <i class="iconly-boldAdd-User"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold">Following</h6>
-                                    <h6 class="font-extrabold mb-0">80.000</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                    <div class="stats-icon red mb-2">
-                                        <i class="iconly-boldBookmark"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold">Saved Post</h6>
-                                    <h6 class="font-extrabold mb-0">112</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="col-12 ">
+            <div class="row" id="stat-cards">
+
             </div>
 
             <div class="row">
-                <div class="col-12">
+                <div class="col-lg-6">
                     <div class="card">
-                        {{-- <div class="card-header">
-                            <h4>TPA</h4>
-                        </div> --}}
                         <div class="card-body">
                             <div id="chart-tpas"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="chart-tpsts"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="chart-tps3rs"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="chart-iplts"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="chart-spalds"></div>
                         </div>
                     </div>
                 </div>
@@ -89,7 +103,7 @@
 
 
         </div>
-        <div class="col-12 col-lg-3">
+        {{-- <div class="col-12 col-lg-3">
             <div class="card">
                 <div class="card-body py-4 px-5">
 
@@ -142,7 +156,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </section>
 @endsection
 @push('js')
@@ -151,7 +165,35 @@
 
     <script>
         $(document).ready(function() {
-            var optionsProfileVisit = {
+            const cardConfig = {
+                tpas_count: {
+                    title: "Total TPA",
+                    color: "tpa",
+                    icon: "iconly-boldShow"
+                },
+                tpsts_count: {
+                    title: "Total TPST",
+                    color: "tpst",
+                    icon: "iconly-boldProfile"
+                },
+                tps3rs_count: {
+                    title: "Total TPS3R",
+                    color: "tps3r",
+                    icon: "iconly-boldAdd-User"
+                },
+                iplts_count: {
+                    title: "Total IPLT",
+                    color: "iplt",
+                    icon: "iconly-boldBookmark"
+                },
+                spalds_count: {
+                    title: "Total SPALD",
+                    color: "spald",
+                    icon: "iconly-boldCategory"
+                }
+            };
+
+            const baseOptionChart = {
                 annotations: {
                     position: "back"
                 },
@@ -166,28 +208,29 @@
                     opacity: 1
                 },
                 series: [],
-                colors: ["#435ebe"],
+                colors: ["#003A70"],
                 xaxis: {
                     categories: []
                 },
-                title: {
-                    text: "Jumlah Data per Kecamatan",
-                    align: "center", // left, center, right
-                    margin: 10,
-                    offsetY: 0,
-                    style: {
-                        fontSize: "16px",
-                        fontWeight: "bold",
-                    }
-                },
-            }
+            };
 
-            var chartProfileVisit = new ApexCharts(
-                document.querySelector("#chart-tpas"),
-                optionsProfileVisit
-            )
+            var chartTpa = new ApexCharts(document.querySelector("#chart-tpas"), JSON.parse(JSON.stringify(
+                baseOptionChart)));
+            var chartTpst = new ApexCharts(document.querySelector("#chart-tpsts"), JSON.parse(JSON.stringify(
+                baseOptionChart)));
+            var chartTps3r = new ApexCharts(document.querySelector("#chart-tps3rs"), JSON.parse(JSON.stringify(
+                baseOptionChart)));
+            var chartIplt = new ApexCharts(document.querySelector("#chart-iplts"), JSON.parse(JSON.stringify(
+                baseOptionChart)));
+            var chartSpald = new ApexCharts(document.querySelector("#chart-spalds"), JSON.parse(JSON.stringify(
+                baseOptionChart)));
 
-            chartProfileVisit.render()
+            chartTpa.render();
+            chartTpst.render();
+            chartTps3r.render();
+            chartIplt.render();
+            chartSpald.render();
+
             getData()
 
             function getData() {
@@ -197,53 +240,135 @@
                     success: function(res) {
 
                         let data = res.data;
+                        let html = "";
 
-                        // ambil nama kecamatan untuk x-axis
-                        let categories = data.map(item => item.nama)
+                        Object.keys(cardConfig).forEach(key => {
+                            let cfg = cardConfig[key];
+                            let total = sumField(data, key);
 
-                        // ambil jumlah tpa
+                            html += createStatCard(cfg.color, cfg.icon, cfg.title, total);
+                        });
+
+                        $("#stat-cards").html(html);
+
+                        // x-axis = nama kecamatan
+                        let categories = data.map(item => item.nama);
+
                         let tpasSeries = data.map(item => item.tpas_count);
                         let tpstsSeries = data.map(item => item.tpsts_count);
                         let tps3rsSeries = data.map(item => item.tps3rs_count);
                         let ipltsSeries = data.map(item => item.iplts_count);
                         let spaldsSeries = data.map(item => item.spalds_count);
 
-                        // update chart
-                        chartProfileVisit.updateOptions({
-                            xaxis: {
-                                categories: categories
+                        // ===================================================================
+                        // CHART TPA
+                        chartTpa.updateOptions({
+                            title: {
+                                text: "TPA per Kecamatan",
+                                align: "center"
                             },
-                            colors: [
-                                "#003A70", // TPAS
-                                "#F7C904", // TPSTS
-                                "#C1272D", // TPS3R
-                                "#4A90E2", // IPLT
-                                "#F5A623" // SPALD
-                            ],
+                            xaxis: {
+                                categories
+                            },
+                            colors: ["#003A70"],
                             series: [{
-                                    name: "TPAS",
-                                    data: tpasSeries
-                                },
-                                {
-                                    name: "TPSTS",
-                                    data: tpstsSeries
-                                },
-                                {
-                                    name: "TPS3R",
-                                    data: tps3rsSeries
-                                },
-                                {
-                                    name: "IPLT",
-                                    data: ipltsSeries
-                                },
-                                {
-                                    name: "SPALD",
-                                    data: spaldsSeries
-                                }
-                            ]
-                        })
+                                name: "TPA",
+                                data: tpasSeries
+                            }]
+                        });
+
+                        // CHART TPST
+                        chartTpst.updateOptions({
+                            title: {
+                                text: "TPST per Kecamatan",
+                                align: "center"
+                            },
+                            xaxis: {
+                                categories
+                            },
+                            colors: ["#F7C904"],
+                            series: [{
+                                name: "TPST",
+                                data: tpstsSeries
+                            }]
+                        });
+
+                        // CHART TPS3R
+                        chartTps3r.updateOptions({
+                            title: {
+                                text: "TPS3R per Kecamatan",
+                                align: "center"
+                            },
+                            xaxis: {
+                                categories
+                            },
+                            colors: ["#C1272D"],
+                            series: [{
+                                name: "TPS3R",
+                                data: tps3rsSeries
+                            }]
+                        });
+
+                        // CHART IPLT
+                        chartIplt.updateOptions({
+                            title: {
+                                text: "IPLT per Kecamatan",
+                                align: "center"
+                            },
+                            xaxis: {
+                                categories
+                            },
+                            colors: ["#4A90E2"],
+                            series: [{
+                                name: "IPLT",
+                                data: ipltsSeries
+                            }]
+                        });
+
+                        // CHART SPALD
+                        chartSpald.updateOptions({
+                            title: {
+                                text: "SPALD per Kecamatan",
+                                align: "center"
+                            },
+                            xaxis: {
+                                categories
+                            },
+                            colors: ["#F5A623"],
+                            series: [{
+                                name: "SPALD",
+                                data: spaldsSeries
+                            }]
+                        });
+
                     }
                 });
+            }
+
+            function createStatCard(color, icon, title, value) {
+                return `
+                    <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-4 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                        <div class="stats-icon ${color} mb-2">
+                                            <i class="${icon}"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                        <h6 class="text-muted font-semibold">${title}</h6>
+                                        <h6 class="font-extrabold mb-0">${value}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+
+            function sumField(data, field) {
+                return data.reduce((acc, item) => acc + item[field], 0);
             }
         });
     </script>
